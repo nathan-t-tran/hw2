72d4f50eec5837a90af1a9d3c855237ea97c28e1
Nathan Tran <nathantt@usc.edu>
2025-02-19 00:14:06 -0800

    updated book file

--- book.h
+++ book.h
@@ -6,7 +6,14 @@
 
 class Book: public Product
 {
+public:
+    Book(string name, double price, int quantity, string category, string author, string ISBN);
+    set<string> keywords();
+    string displayString();
 
+private:
+    string author;
+    string ISBN;
 };
 
 #endif
